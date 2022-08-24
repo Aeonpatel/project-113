6ecca128d610199e8769435c6cfee4456eee1a6c
Aeonpatel <85121945+Aeonpatel@users.noreply.github.com>
2022-08-24 21:38:47 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+canvas=document.getElementById("canvas").value;
+function preload(){
+}
+
+
+function take_snapshot() {
+save("snap.png");
+}
+
+function setup(){
+canvas.width="300";
+canvas.height="300";
+canvas.center();
+}
+
+function draw(){
+
+}
